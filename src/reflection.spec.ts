c72c816caf3ef79999cc814beef21ed052c0db12
Mark Wecke <wecke@neoskop.de>
2018-07-10 10:26:32 +0200

    tests: fix reflection tests

--- src/reflection.spec.ts
+++ src/reflection.spec.ts
@@ -25,30 +25,40 @@ class D extends B {
 
 describe('Reflection', () => {
     describe('getParentClasses', () => {
-        expect(Reflection.getParentClasses(A)).to.be.eql([ Object ]);
-        expect(Reflection.getParentClasses(B)).to.be.eql([ A, Object ]);
-        expect(Reflection.getParentClasses(C)).to.be.eql([ B, A, Object ]);
-        expect(Reflection.getParentClasses(D)).to.be.eql([ B, A, Object ]);
+        it('should return all parent classes' , () => {
+            expect(Reflection.getParentClasses(A)).to.be.eql([ Object ]);
+            expect(Reflection.getParentClasses(B)).to.be.eql([ A, Object ]);
+            expect(Reflection.getParentClasses(C)).to.be.eql([ B, A, Object ]);
+            expect(Reflection.getParentClasses(D)).to.be.eql([ B, A, Object ]);
+        });
     });
     
     describe('getOwnClassMethods', () => {
-        expect(Reflection.getOwnClassMethods(A)).to.be.eql([ 'methodA' ]);
-        expect(Reflection.getOwnClassMethods(B)).to.be.eql([ 'methodB' ]);
+        it('should return all own class methods', () => {
+            expect(Reflection.getOwnClassMethods(A)).to.be.eql([ 'methodA' ]);
+            expect(Reflection.getOwnClassMethods(B)).to.be.eql([ 'methodB' ]);
+        });
     });
     
     describe('getAllClassMethods', () => {
-        expect(Reflection.getAllClassMethods(A)).to.be.eql([ 'methodA' ]);
-        expect(Reflection.getAllClassMethods(B)).to.be.eql([ 'methodB', 'methodA' ]);
+        it('should return all class methods', () => {
+            expect(Reflection.getAllClassMethods(A)).to.be.eql([ 'methodA' ]);
+            expect(Reflection.getAllClassMethods(B)).to.be.eql([ 'methodB', 'methodA' ]);
+        });
     });
     
     describe('getOwnStaticClassMethods', () => {
-        expect(Reflection.getOwnStaticClassMethods(A)).to.be.eql([]);
-        expect(Reflection.getOwnStaticClassMethods(B)).to.be.eql([ 'staticB' ]);
+        it('should return all own static class methods', () => {
+            expect(Reflection.getOwnStaticClassMethods(A)).to.be.eql([]);
+            expect(Reflection.getOwnStaticClassMethods(B)).to.be.eql([ 'staticB' ]);
+        });
     });
     
     describe('getAllStaticClassMethods', () => {
-        expect(Reflection.getAllStaticClassMethods(A)).to.be.eql([]);
-        expect(Reflection.getAllStaticClassMethods(B)).to.be.eql([ 'staticB' ]);
-        expect(Reflection.getAllStaticClassMethods(C)).to.be.eql([ 'staticC', 'staticB' ]);
+        it('should return all static class methods', () => {
+            expect(Reflection.getAllStaticClassMethods(A)).to.be.eql([]);
+            expect(Reflection.getAllStaticClassMethods(B)).to.be.eql([ 'staticB' ]);
+            expect(Reflection.getAllStaticClassMethods(C)).to.be.eql([ 'staticC', 'staticB' ]);
+        });
     });
 });
